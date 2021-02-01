4e09582cba4646f9bfa108740229bee5903638c5
kimlai <hello@kimlaitrinh.me>
2021-02-01 12:37:41 +0100

    initial commit

---
+++ exo-login/main.js
@@ -0,0 +1,6 @@
+document.querySelector("form").addEventListener("submit", () => {
+  // avant d'envoyer les données au serveur, on peut faire une étape de validation
+
+  // on n'envoie pas vraiment les données pour l'exercice
+  document.querySelector("main").innerHTML = "Vous êtes connecté :)";
+});
