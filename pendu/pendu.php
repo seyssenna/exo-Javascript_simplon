3c001dfcf8a4b97feb77d5c46171ac4c115a16a9
kimlai <hello@kimlaitrinh.me>
2021-02-15 18:49:19 +0100

    add pendu

---
+++ pendu/pendu.php
@@ -0,0 +1,47 @@
+<?php
+
+$word = "abracadabra";
+$wrongLetters = [];
+$goodLetters = [];
+$guessed = false;
+
+while (!$guessed && count($wrongLetters) < 5) {
+  $guess = readline("Entrez une lettre : ");
+
+  if (strpos($word, $guess) === false) {
+    if (!in_array($guess, $wrongLetters)) {
+      $wrongLetters[] = $guess;
+    }
+  } else {
+    if (!in_array($guess, $goodLetters)) {
+      $goodLetters[] = $guess;
+    }
+  }
+
+  foreach($wrongLetters as $wrongLetter) {
+    echo "$wrongLetter ";
+  }
+  echo "\n";
+
+  $allLetters = true;
+  foreach(str_split($word) as $letter) {
+    if (in_array($letter, $goodLetters)) {
+      echo "$letter ";
+    } else {
+      $allLetters = false;
+      echo "_ ";
+    }
+  }
+  echo "\n";
+
+  if ($allLetters) {
+    $guessed = true;
+  }
+}
+
+if (count($wrongLetters) == 5) {
+  echo "Perdu !\n";
+}
+if ($guessed) {
+  echo "Gagné !\n";
+}
