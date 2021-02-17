8265da5c16881aa6a5c089a48e8f42f25fc1dcef
kimlai <hello@kimlaitrinh.me>
2021-02-17 11:56:46 +0100

    live coded pendu final version

--- pendu/pendu2.php
+++ pendu/pendu2.php
@@ -1,9 +1,10 @@
 <?php
 
 $word = "tout";
-$splitWord = str_split($word);
-$bars = [];
+$splitWord = str_split($word); // ["t", "o", "u", "t"]
 
+// créer un tableau qui contient un "_" par lettre du mot à deviner
+$bars = [];
 for ($i = 0; $i < count($splitWord); $i++) {
   $bars[] = "_";
 }
@@ -11,21 +12,36 @@ for ($i = 0; $i < count($splitWord); $i++) {
 echo implode(" ", $bars);
 echo "\n\n";
 
-// bars <- ["t", " ", "o", " ", "u", " ", "t"]
-// splitWord <- ["t", "o", "u", "t"]
-// word <- "tout"
-// implode("", $bars) <- "t o u t"
-while (implode($bars) !== $word) {
+$fail = [];
+
+// tant que le joueur n'a ni gagné, ni perdu, on joue
+while (implode($bars) !== $word && count($fail) < 4) {
+  // on récupère la lettre proposée par le joueur
   $guessedLetter = readline("Entrez une lettre : ");
 
+  // si la lettre proposée est dans le mot à deviner
+  // on remplace les "_" par cette lettre
   if (in_array($guessedLetter, $splitWord) === true) {
     $positions = array_keys($splitWord, $guessedLetter);
     foreach ($positions as $position) {
       $bars[$position] = $guessedLetter;
     }
-    echo implode($bars, " ");
+    echo implode(" ", $bars);
     echo "\n";
   } else {
-    echo "non\n";
+    // si le joueur propose deux fois la même lettre, on ne
+    // compte qu'une seule erreur
+    if (!in_array($guessedLetter, $fail)) {
+      $fail[] = $guessedLetter;
+    }
+    echo implode(";", $fail);
+    echo "\n";
   }
+}
+
+// si le joueur a gagné on affiche "gagné", sinon "perdu"
+if(implode($bars) === $word) {
+  echo "Gagné !\n";
+} else {
+  echo "Perdu !\n";
 }
